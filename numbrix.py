a49865a996cfb21a9fbdf7359a9463d74f7586aa
FunkyCracky <futebol4@hotmail.com>
2022-03-10 10:10:08 +0000

    First functions

---
+++ numbrix.py
@@ -0,0 +1,121 @@
+# numbrix.py: Template para implementação do projeto de Inteligência Artificial 2021/2022.
+# Devem alterar as classes e funções neste ficheiro de acordo com as instruções do enunciado.
+# Além das funções e classes já definidas, podem acrescentar outras que considerem pertinentes.
+
+# Grupo 00:
+# 00000 Nome1
+# 00000 Nome2
+
+import sys
+from search import Problem, Node, astar_search, breadth_first_tree_search, depth_first_tree_search, greedy_search, recursive_best_first_search
+
+
+class NumbrixState:
+    state_id = 0
+
+    def __init__(self, board):
+        self.board = board
+        self.id = NumbrixState.state_id
+        NumbrixState.state_id += 1
+
+    def __lt__(self, other):
+        return self.id < other.id
+        
+    # TODO: outros metodos da classe
+
+
+class Board:
+    """ Representação interna de um tabuleiro de Numbrix. """
+
+    def __init__(self, board: list) -> None:
+        self._board = board
+
+    def get_number(self, row: int, col: int) -> int:
+        """ Devolve o valor na respetiva posição do tabuleiro. """
+        return self._board[row][col]
+    
+    def adjacent_vertical_numbers(self, row: int, col: int) -> (int, int):
+        """ Devolve os valores imediatamente abaixo e acima, 
+        respectivamente. """
+        numbers = ()
+        for y in (col-1, col+1):
+            try:
+                numbers += (self.get_number(row, y),)
+            except IndexError:
+                numbers += (None,)
+        return numbers
+
+    
+    def adjacent_horizontal_numbers(self, row: int, col: int) -> (int, int):
+        """ Devolve os valores imediatamente à esquerda e à direita, 
+        respectivamente. """
+        numbers = ()
+        for x in (row-1, row+1):
+            try:
+                numbers += (self.get_number(x, col),)
+            except IndexError:
+                numbers += (None,)
+        return numbers
+    
+    @staticmethod    
+    def parse_instance(filename: str):
+        """ Lê o ficheiro cujo caminho é passado como argumento e retorna
+        uma instância da classe Board. """
+        # TODO
+        with open(filename, "r") as f:
+            lines = f.readlines()
+            dim = int(lines[0])
+            lines = lines[1:]
+            assert(len(lines) == dim)
+            board = []
+            for line in lines:
+                line = line.split("\t")
+                assert(len(line) == dim)
+                board.append([int(x) for x in line])
+        return Board(board)
+
+    # TODO: outros metodos da classe
+
+
+class Numbrix(Problem):
+    def __init__(self, board: Board):
+        """ O construtor especifica o estado inicial. """
+        # TODO
+        pass
+
+    def actions(self, state: NumbrixState):
+        """ Retorna uma lista de ações que podem ser executadas a
+        partir do estado passado como argumento. """
+        # TODO
+        pass
+
+    def result(self, state: NumbrixState, action):
+        """ Retorna o estado resultante de executar a 'action' sobre
+        'state' passado como argumento. A ação a executar deve ser uma
+        das presentes na lista obtida pela execução de 
+        self.actions(state). """
+        # TODO
+        pass
+
+    def goal_test(self, state: NumbrixState):
+        """ Retorna True se e só se o estado passado como argumento é
+        um estado objetivo. Deve verificar se todas as posições do tabuleiro 
+        estão preenchidas com uma sequência de números adjacentes. """
+        # TODO
+        pass
+
+    def h(self, node: Node):
+        """ Função heuristica utilizada para a procura A*. """
+        # TODO
+        pass
+    
+    # TODO: outros metodos da classe
+
+if __name__ == "__main__":
+    # TODO:
+    # Ler o ficheiro de input de sys.argv[1],
+    # Usar uma técnica de procura para resolver a instância,
+    # Retirar a solução a partir do nó resultante,
+    # Imprimir para o standard output no formato indicado.
+    board = Board.parse_instance(sys.argv[1])
+    pass
