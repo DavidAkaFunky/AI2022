3ffe27b17be999e0e60dadb6decb9c920ab93b6a
FunkyCracky <futebol4@hotmail.com>
2022-03-19 15:10:44 +0000

    More stuff added

--- numbrix.py
+++ numbrix.py
@@ -2,9 +2,9 @@
 # Devem alterar as classes e funções neste ficheiro de acordo com as instruções do enunciado.
 # Além das funções e classes já definidas, podem acrescentar outras que considerem pertinentes.
 
-# Grupo 00:
-# 00000 Nome1
-# 00000 Nome2
+# Grupo 03:
+# 95550 David Belchior
+# 95562 Diogo Santos
 
 import sys
 from search import Problem, Node, astar_search, breadth_first_tree_search, depth_first_tree_search, greedy_search, recursive_best_first_search
@@ -23,26 +23,44 @@ class NumbrixState:
         
     # TODO: outros metodos da classe
 
+    def get_board(self):
+        return self.board
+
 
 class Board:
     """ Representação interna de um tabuleiro de Numbrix. """
 
-    def __init__(self, board: list) -> None:
+    def __init__(self, board: list, dim: int) -> None:
         self._board = board
+        self._dim = dim
+
+    def get_board(self) -> list:
+        return self._board
+
+    def get_dim(self) -> int:
+        return self._dim
 
     def get_number(self, row: int, col: int) -> int:
         """ Devolve o valor na respetiva posição do tabuleiro. """
         return self._board[row][col]
+
+    def get_number_seq(self, number: int) -> tuple:
+        res = ()
+        if number > 1:
+            res += (number-1,)
+        if number < self._dim**2:
+            res += (number+1,)
+        return res
     
     def adjacent_vertical_numbers(self, row: int, col: int) -> (int, int):
         """ Devolve os valores imediatamente abaixo e acima, 
         respectivamente. """
         numbers = ()
         for x in (row-1, row+1):
-            try:
-                numbers += (self.get_number(x, col),)
-            except IndexError:
+            if x < 0 or x >= self._dim:
                 numbers += (None,)
+            else:
+                numbers += (self.get_number(x, col),)
         return numbers
 
     
@@ -51,12 +69,18 @@ class Board:
         respectivamente. """
         numbers = ()
         for y in (col-1, col+1):
-            try:
-                numbers += (self.get_number(row, y),)
-            except IndexError:
+            if y < 0 or y >= self._dim:
                 numbers += (None,)
+            else:
+                numbers += (self.get_number(row, y),)
         return numbers
     
+    def get_neighbours(self, row: int, col: int) -> tuple:
+        neighbours = self.adjacent_vertical_numbers(row, col) + self.adjacent_horizontal_numbers(row, col)
+        return tuple(filter(lambda n: n != None, neighbours))
+    
+    def add_number(self, number: int, row: int, col: int) -> None:
+        self._board[row][col] = number
 
     def print_board(self):
         """ Imprime o tabuleiro na consola. """
@@ -75,10 +99,10 @@ class Board:
             assert(len(lines) == dim)
             board = []
             for line in lines:
-                line = line.split("\t")
+                line = line.strip().split("\t")
                 assert(len(line) == dim)
                 board.append([int(x) for x in line])
-        return Board(board)
+        return Board(board, dim)
 
 
 
@@ -88,12 +112,13 @@ class Board:
 class Numbrix(Problem):
     def __init__(self, board: Board):
         """ O construtor especifica o estado inicial. """
-        # TODO
+        self.board = board
         pass
 
     def actions(self, state: NumbrixState):
         """ Retorna uma lista de ações que podem ser executadas a
         partir do estado passado como argumento. """
+        # action -> tuplo (2,1,8) -> “preenchero número 8 na posição linha 2 coluna 1
         # TODO
         pass
 
@@ -102,18 +127,33 @@ class Numbrix(Problem):
         'state' passado como argumento. A ação a executar deve ser uma
         das presentes na lista obtida pela execução de 
         self.actions(state). """
+        # dada uma acao e um estado retorna o resultado de aplicar essa acao a esse estado
         # TODO
+        state.get_board().add_number(action[0],action[1],action[2])
         pass
 
     def goal_test(self, state: NumbrixState):
         """ Retorna True se e só se o estado passado como argumento é
         um estado objetivo. Deve verificar se todas as posições do tabuleiro 
         estão preenchidas com uma sequência de números adjacentes. """
-        # TODO
-        pass
+
+        board = state.board
+        dim = board.get_dim()
+
+        for row in range(dim):
+            for col in range(dim):
+                neighbours = board.get_neighbours(row, col)
+                number_seq = board.get_number_seq(board.get_number(row, col))
+                for number in number_seq:
+                    if number not in neighbours:
+                        return False 
+      
+        return True
+        
 
     def h(self, node: Node):
         """ Função heuristica utilizada para a procura A*. """
+        # estado atual -> node.state
         # TODO
         pass
     
@@ -127,4 +167,8 @@ if __name__ == "__main__":
     # Imprimir para o standard output no formato indicado.
     board = Board.parse_instance(sys.argv[1])
     board.print_board()
-    pass
+    problem = Numbrix(board)
+    initial_state = NumbrixState(board)
+
+    print("Is goal?", problem.goal_test(initial_state))
+    n = board.get_dim()
